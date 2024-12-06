b9d5afec4e96333968d58054e970e0fb3d96fdb6
Alfajr3333 <marioabdo476@gmail.com>
2024-12-06 11:13:19 +0200

    commit

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(express.json()); // To parse JSON data
 
 // MongoDB connection
-mongoose.connect(process.env.MONGO_URI, {
+mongoose.connect("mongodb://localhost:27017/mydb", {
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
